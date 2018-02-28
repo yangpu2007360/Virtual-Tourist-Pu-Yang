d6673e7c30044f6f394059c56fcaf7c87b917867
pu yang <yangpu2007360@gmail.com git config --global user.name Pu>
2018-02-27 17:58:02 -0800

    Initial Commit

---
+++ Virtual Tourist, Pu Yang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Virtual Tourist, Pu Yang
+//
+//  Created by pu yang on 2/27/18.
+//  Copyright © 2018 pu yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
